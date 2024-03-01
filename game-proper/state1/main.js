c6cd436819666d6265ea8adcbae01e8cce069eec
Qhaleed <reslykadiri@gmail.com>
2024-03-01 11:51:41 +0800

    animation for  the "teleporting Screen"

--- game-proper/state1/main.js
+++ game-proper/state1/main.js
@@ -1,7 +1,7 @@
 const section1 = document.getElementById('section-1');
 const section2 = document.getElementById('section-2');
-const description1 = document.getElementById('description1');
-const description2 = document.getElementById('description2');
+const description1 = document.getElementById('description2');
+const description2 = document.getElementById('description1');
 const aurora = document.getElementById('faction-A');
 const umbral = document.getElementById('faction-B');
 
@@ -9,10 +9,10 @@ const umbral = document.getElementById('faction-B');
 // SECTION 1 HOVER
 section1.addEventListener("mouseover", function () {
     setTimeout(() => {
-        description2.style.opacity = '0.01';
+        description1.style.opacity = '0.01';
     }, 200);
     setTimeout(() => {
-        description2.style.opacity = '1';
+        description1.style.opacity = '1';
         
      
     }, 400);
@@ -21,11 +21,11 @@ section1.addEventListener("mouseover", function () {
 
 section1.addEventListener("mouseout", function () {
     setTimeout(() => {
-        description2.style.opacity = '1';
+        description1.style.opacity = '1';
     }, 200);
    
     setTimeout(() => {
-        description2.style.opacity = '0.01';
+        description1.style.opacity = '0.01';
      
     }, 400);
 })
@@ -33,10 +33,10 @@ section1.addEventListener("mouseout", function () {
 // SECTION 2 HOVER
 section2.addEventListener("mouseover", function () {
     setTimeout(() => {
-        description1.style.opacity = '0.01';
+        description2.style.opacity = '0.01';
     }, 200);
     setTimeout(() => {
-        description1.style.opacity = '1';
+        description2.style.opacity = '1';
         
      
     }, 400);
@@ -45,11 +45,11 @@ section2.addEventListener("mouseover", function () {
 
 section2.addEventListener("mouseout", function () {
     setTimeout(() => {
-        description1.style.opacity = '1';
+        description2.style.opacity = '1';
     }, 200);
    
     setTimeout(() => {
-        description1.style.opacity = '0.01';
+        description2.style.opacity = '0.01';
      
     }, 400);
 })
@@ -59,12 +59,20 @@ section2.addEventListener("mouseout", function () {
 
 // THE AURORA FACTION
 aurora.addEventListener('click'  ,function(){
-    aurora.textContent='Faction Chosen';
+    aurora.textContent='Faction Chosen...';
+    description1.textContent = 'Game is updating...';
+    setTimeout(() => {
+        description1.textContent = 'Game is updated... You will now be teleported to your homeworld';
+    }, 2000);
+    setTimeout(() => {
+        section2.style.display = 'none';
+        section1.style.width = '100%';
+    }, 3000);
 })
 
 // THE UMBRAL FACTION
 
 umbral.addEventListener('click', function () {
     umbral.textContent = "Coming Soon!";
-    description1.textContent = "Apologies for the inconvience, still working on it";
+    description2.textContent = "Apologies for the inconvience, still working on it";
 })
