1b6d141b18b98cf6867c83292d6f8ca1b45ad934
Qhaleed <reslykadiri@gmail.com>
2024-03-01 08:45:05 +0800

    Index added
    Trying to connect and make this thing work

---
+++ game-proper/main.js
@@ -0,0 +1,77 @@
+const usernameInput = document.getElementById("username");
+const typeInput = document.getElementById("password");
+const identityInput = document.getElementById("email");
+const submitButton = document.getElementById("submitbtn");
+const submitButtonNew = document.getElementById("submitbtnNew");
+const worldHeader = document.getElementById('world-header');
+const form = document.getElementById('loginForm');
+
+const accounts = [
+    {
+        username: 'user1', 
+        type: 'pass1', 
+        identity: 'user1@example.com'
+    },
+    {
+        username: 'user2', 
+        type: 'pass2', 
+        identity: 'user2@example.com'
+    }
+];
+
+form.addEventListener ('submit', function (event) {
+    event.preventDefault();
+    authentication();
+});
+// For user credential authentication
+
+function authentication () {
+    const username = usernameInput.value.trim();
+    const type = typeInput.value.trim();
+    const identity = identityInput.value.trim();
+
+    const account = accounts.find(account => account.username == username && account.type == type);
+    if (account) {
+        worldHeader.textContent = 'Game is updating...';
+        setTimeout(() => {
+            window.location.href ='state1/index.html';
+        }, 5000);
+      
+        
+    } else {
+        worldHeader.textContent = 'Invalid Login... User does not exist';
+    }
+    usernameInput = '';
+    typeInput = '';
+    identityInput = '';
+}   
+
+submitButton.addEventListener("click", authentication);
+
+// Account creation function
+// function createAccount () {
+//     const usernameNew = usernameInput.value.trim();
+//     const typeNew = typeInput.value.trim();
+//     const identityNew = identityInput.value.trim();
+
+//     //check for existing accounts
+
+//     const existingAccount = accounts.find(account => account.username == usernameNew);
+//     if (existingAccount){
+//         worldHeader.textContent = "Username already exists...";
+//         return;
+//     }
+
+//     const newAccount = {
+//         username: usernameNew,
+//         password: typeNew,
+//         email: identityNew
+
+//     };
+//     accounts.push(newAccount);
+//     alert("Account Saved");
+
+//     console.log(newAccount);
+// }
+
+// submitButtonNew.addEventListener("click", createAccount);
---
+++ main.js
@@ -0,0 +1,53 @@
+var navbar = document.getElementById('navbar');
+const enterGameButton = document.getElementById('enterGameButton');
+const land = document.getElementById('landing-page')
+const mainMenu = document.getElementById('main-menu');
+const loadingScreen = document.getElementById('loading-screen');
+const loadingHeader = document.getElementById('header');
+const input = document.getElementById('input');
+const submitBtn = document.getElementById('submit-btn');
+const mainText = document.getElementById('main-text'); 
+// The navbar functions
+window.onscroll = function() {scrollFunction()};
+function scrollFunction () {
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop >20) {
+        navbar.style.backgroundColor = "rgba(0,0,0,0.8)";
+        navbar.style.transition = "500ms"
+    }
+    else {
+        navbar.style.backgroundColor = " rgba(245, 245, 220, 0)";
+    }
+}
+
+// Entergame funtion
+
+enterGameButton.addEventListener("click", function enterGame () {
+
+        setTimeout(() => {
+            
+            land.style.display = 'none';
+            loadingScreen.style.display = 'block';
+            mainMenu.style.display = 'none';
+        }, 2000);
+        setTimeout(() => {
+           
+           window.location.href ='game-proper/index.html';
+        }, 5000);
+    
+    
+});
+
+
+
+
+
+
+// Lets try to define game logic
+
+const game = {
+    start: function () {
+        output("Welcome to the game");
+    },
+    
+}
+
